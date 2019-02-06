3abd16722637165ec211238c1517ae8e2f0392d4
GaetanGalati <32631949+GaetanGalati@users.noreply.github.com>
2019-02-06 20:55:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,16 @@
 #include <windows.h>
 #include "GameOfLife_Head.h"
 
-
+//**********************************************************
+//Description : Le main.
+//
+//Entrées : X
+//
+//Sorties :	Une boucle "for" qui retourne l'affichage du jeu de la vie jussqu'a se qu'elle soit au nombre max de génération
+//
+//Note :  Parfois quand on le compile le jeu ne se lance pas, il faut juste recompilé
+//
+//**********************************************************
 int main()
 {
     srand(time(NULL));
@@ -19,7 +28,7 @@ int main()
     {
             Display(nTab);
             printf("\nGeneration : %d\n",i);
-            fprintf(fichier, "\nGeneration : %d\n",i);
+            fprintf(fichier, "\nGeneration : %d\n",i);/
             genCells(nTab,Cell,fichier);
             Sleep(500);
             system("cls");
